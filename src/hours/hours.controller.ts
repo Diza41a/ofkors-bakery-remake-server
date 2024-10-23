9668d81f8a116140bcc209fdb59b6b971d5499d9
Davyd Zakorchennyi <Diza41a@mail.com>
2024-10-23 16:07:08 -0500

    @feat: Hours - Removed console logs;

--- src/hours/hours.controller.ts
+++ src/hours/hours.controller.ts
@@ -26,11 +26,9 @@ export class HoursOfOperationController {
     try {
       const pathname = path.join(__dirname, '../../src/hours/hours_of_operation.json');
       fs.writeFileSync(pathname, JSON.stringify(updatedHours, null, 2));
-      console.log('Hours of operation updated successfully');
       return updatedHours;
     }
     catch (error) {
-      console.log(`Error writing hours of operation to file: ${error.message}`);
       throw new Error(`Error writing hours of operation to file: ${error.message}`);
     }
   }
