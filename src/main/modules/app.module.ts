ed25692b11dd5a42fa4e8e4d59df19e8aed424ab
Davyd Zakorchennyi <Diza41a@mail.com>
2024-10-28 17:38:24 -0500

    @fix: Vercel - 'src/...pathToFile' -> '../...pathToFile';

--- src/main/modules/app.module.ts
+++ src/main/modules/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from 'src/auth/modules/auth.module';
+import { AuthModule } from '../../auth/modules/auth.module';
 import { MenuItemModule } from '../../menuItems/menuItem.module';
-import { HoursOfOperationModule } from 'src/hours/hours.module';
+import { HoursOfOperationModule } from '../../hours/hours.module';
 
 @Module({
   imports: [AuthModule, MenuItemModule, HoursOfOperationModule],
